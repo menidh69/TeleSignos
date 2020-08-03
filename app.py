55054ea0e6c0453ecd52c2368b6853a7fc59c5a6
Manuel Duarte <manuelduarteh@gmail.com>
2020-08-03 14:46:21 -0700

    Added relationships to db model, migrated db to local

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Municipio
+from models import *
 
 
 @app.route('/')
--- models.py
+++ models.py
@@ -1,12 +1,34 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
+import enum
 
+class UrgenciaEnum(enum.Enum):
+    EMBARAZO='embarazo'
+    ACCIDENTE='accidente'
+    RESPIRATORIO='respiratorio'
+    OTRO='otro'
+
+class Gravedad(enum.Enum):
+    BAJA='baja'
+    MEDIA='media'
+    ALTA='alta'
+    MUY_ALTA='muy alta'
+
+class tipoUsuario(enum.Enum):
+    PARAMEDICO='paramédico'
+    MEDICO_REG='medico regulador'
+    REGISTRANTE='registrante'
+    ADMIN='admin'
 
 class Municipio(db.Model):
     __tablename__ = 'municipios'
+    __table_args__ = {"schema": "public"}
 
     id_municipio = db.Column(db.Integer, primary_key=True)
     nombre_municipio = db.Column(db.String(70))
+    colonias = db.relationship('Colonia', backref='municipio', lazy=True)
+    hospitales = db.relationship('Hospital', backref='municipio', lazy=True)
+
 
     def __init__(self, nombre_municipio):
         self.nombre_municipio = nombre_municipio
@@ -16,10 +38,13 @@ class Municipio(db.Model):
 
 class Colonia(db.Model):
     __tablename__ = 'colonias'
+    __table_args__ = {"schema": "public"}
 
     id_colonia = db.Column(db.Integer, primary_key=True)
     nombre_colonia = db.Column(db.String(70))
-    id_municipio = db.Column(db.Integer)
+    id_municipio = db.Column(db.Integer, db.ForeignKey('public.municipios.id_municipio'), nullable=False)
+    pacientes = db.relationship('Paciente', backref='colonia', lazy=True)
+
 
     def __init__(self, nombre_colonia):
         self.nombre_colonia= nombre_colonia
@@ -29,13 +54,15 @@ class Colonia(db.Model):
 
 class Hospital(db.Model):
     __tablename__ = 'hospitales'
+    __table_args__ = {"schema": "public"}
 
-    id_hospital = db.Column(db.Integer, primary_key=True)
-    id_municipio = db.Column(db.Integer)
+    id_hospital = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_municipio = db.Column(db.Integer, db.ForeignKey('public.municipios.id_municipio'), nullable=False)
     nombre_hospital = db.Column(db.String(70))
     direccion = db.Column(db.String(70))
     telefono = db.Column(db.String(10))
     email = db.Column(db.String(70))
+    movimientos = db.relationship('Movimiento', backref='hospital', lazy=True)
 
     def __init__(self, nombre_hospital, direccion, telefono, email):
         self.nombre_hospital = nombre_hospital
@@ -46,28 +73,16 @@ class Hospital(db.Model):
     def __repr__(self):
         return '<id_hospital {}>'.format(self.id_hospital)
 
-class Ambulancia(db.Model):
-    __tablename__ = 'ambulancias'
-
-    id_ambulancia = db.Column(db.Integer, primary_key=True)
-    num_unidad = db.Column(db.String(10))
-    id_servicio = db.Column(db.Integer)
-
-    def __init__(self, num_unidad, id_servicio):
-        self.num_unidad= num_unidad
-        self.id_servicio = id_servicio
-
-    def __repr__(self):
-        return '<id_ambulancia {}>'.format(self.id_ambulancia)
-
 class Servicio(db.Model):
     __tablename__ = 'servicios'
+    __table_args__ = {"schema": "public"}
 
     id_servicio = db.Column(db.Integer, primary_key=True)
     servicio_nombre = db.Column(db.String(10))
     contacto = db.Column(db.String(70))
     telefono = db.Column(db.String(10))
     email = db.Column(db.String(70))
+    ambulancias = db.relationship('Ambulancia', backref='servicio', lazy=True)
 
     def __init__(self, servicio_nombre, contacto, telefono, email):
         self.servicio_nombre = servicio_nombre
@@ -78,29 +93,32 @@ class Servicio(db.Model):
     def __repr__(self):
         return '<id {}>'.format(self.id_servicio)
 
-class Usuario(db.Model):
-    __tablename__ = 'usuarios'
+class Ambulancia(db.Model):
+    __tablename__ = 'ambulancias'
+    __table_args__ = {"schema": "public"}
 
-    id_usuario = db.Column(db.Integer, primary_key=True)
-    nombre_usuario = db.Column(db.String(70))
-    id_tipo_usuario = db.Column(db.Integer)
-    
+    id_ambulancia = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    num_unidad = db.Column(db.String(10))
+    id_servicio = db.Column(db.Integer, db.ForeignKey('public.servicios.id_servicio'), nullable=False)
+    movimientos = db.relationship('Movimiento', backref='ambulancia', lazy=True)
 
-    def __init__(self, nombre_usuario, id_tipo_usuario):
-        self.nombre_usuario = nombre_usuario
-        self.id_tipo_usuario
+
+    def __init__(self, num_unidad):
+        self.num_unidad= num_unidad
+        
 
     def __repr__(self):
-        return '<id_usuario {}>'.format(self.id_usuario)
-    
+        return '<id_ambulancia {}>'.format(self.id_ambulancia)
+
 class Tipo_Usuario(db.Model):
     __tablename__ = 'tipo_usuario'
+    __table_args__ = {"schema": "public"}
 
-    id_tipo_usuario = db.Column(db.Integer, primary_key=True)
-    tipo_usuario = db.Column(db.String(70))
+    id_tipo_usuario = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    tipo_usuario = db.Column(db.Enum(tipoUsuario))
     registro = db.Column(db.Boolean)
     medico_reg = db.Column(db.Boolean)
-    
+    usuarios = db.relationship('Usuario', backref='tipo_usuario', lazy=True)
 
     def __init__(self, tipo_usuario, registro, medico_reg):
         self.tipo_usuario = tipo_usuario
@@ -110,16 +128,35 @@ class Tipo_Usuario(db.Model):
     def __repr__(self):
         return '<id_tipo_usuario {}>'.format(self.id_tipo_usuario)
 
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    __table_args__ = {"schema": "public"}
+
+    id_usuario = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nombre_usuario = db.Column(db.String(70))
+    id_tipo_usuario = db.Column(db.Integer, db.ForeignKey('public.tipo_usuario.id_tipo_usuario'), nullable=False)
+    movimientos = db.relationship('Movimiento', backref='usuario', lazy=True)
+
+
+    def __init__(self, nombre_usuario, id_tipo_usuario):
+        self.nombre_usuario = nombre_usuario
+        self.id_tipo_usuario
+
+    def __repr__(self):
+        return '<id_usuario {}>'.format(self.id_usuario)
+
 class Paciente(db.Model):
     __tablename__ = 'pacientes'
+    __table_args__ = {"schema": "public"}
 
-    id_paciente = db.Column(db.Integer, primary_key = True)
+    id_paciente = db.Column(db.Integer, primary_key = True, autoincrement=True)
     servicio_medico = db.Column(db.String(70))
     nombre_paciente = db.Column(db.String(70))
     apellidos = db.Column(db.String(100))
     genero = db.Column(db.String(1))
     fecha_nac = db.Column(db.DateTime)
-    id_colonia = db.Column(db.Integer)
+    id_colonia = db.Column(db.Integer, db.ForeignKey('public.colonias.id_colonia'), nullable=False)
+    movimientos = db.relationship('Movimiento', backref='paciente', lazy=True)
   
     def __init__(self, servicio_medico, nombre_paciente, apellidos, genero, fecha_nac, id_colonia):
         self.servicio_medico = servicio_medico
@@ -132,16 +169,33 @@ class Paciente(db.Model):
     def __repr__(self):
         return '<id_paciente {}>'.format(self.id_paciente)
 
-class Movimientos(db.Model):
-    __tablename__ = 'movimientos'
+class Tipo_Urgencia(db.Model):
+    __tablename__= 'tipo_urgencia'
+    __table_args__ = {'schema': 'public'}
 
-    id_movimiento = db.Column(db.Integer, primary_key=True)
-    id_paciente = db.Column(db.Integer)
-    id_usuario = db.Column(db.Integer)
-    id_hospital = db.Column(db.Integer)
-    id_ambulancia = db.Column(db.Integer)
-    id_colonia = db.Column(db.Integer)
-    id_urgencia = db.Column(db.Integer)
+    id_tipo_urgencia = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    urgencia = db.Enum(UrgenciaEnum)
+    descripcion = db.Column(db.String(70))
+    movimientos = db.relationship('Movimiento', backref='tipo_urgencia', lazy=True)
+
+    def __init__(self, urgencia, descripcion):
+        self.urgencia = urgencia
+        self.descripcion = descripcion
+
+    def __repr__(self):
+        return '<id_tipo_urgencia {}>'.format(self.id_tipo_urgencia)
+
+
+class Movimiento(db.Model):
+    __tablename__ = 'movimientos'
+    __table_args__ = {"schema": "public"}
+
+    id_movimiento = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_paciente = db.Column(db.Integer, db.ForeignKey('public.pacientes.id_paciente'), nullable=False)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('public.usuarios.id_usuario'), nullable=False)
+    id_hospital = db.Column(db.Integer, db.ForeignKey('public.hospitales.id_hospital'), nullable=False)
+    id_ambulancia = db.Column(db.Integer, db.ForeignKey('public.ambulancias.id_ambulancia'), nullable=False)
+    id_tipo_urgencia = db.Column(db.Integer, db.ForeignKey('public.tipo_urgencia.id_tipo_urgencia'), nullable=False)
     fecha_inicio = db.Column(db.DateTime)
     fecha_final = db.Column(db.DateTime)
     presion_arterial = db.Column(db.String(10))
@@ -149,7 +203,9 @@ class Movimientos(db.Model):
     frec_respiratoria = db.Column(db.String(10))
     temperatura = db.Column(db.String(2))
     escala_glassgow = db.Column(db.String(10))
-    gravedad = db.Column(db.String(10))
+    gravedad = db.Column(db.Enum(Gravedad))
+    registros = db.relationship('Bitacora', backref='movimientos', lazy=True)
+    
 
     def __init__(self, id_movimiento, id_paciente, id_usuario, id_hospital, 
     id_ambulancia, id_colonia, id_urgencia, fecha_inicio, fecha_final, presion_arterial, frec_cardiaca, frec_respiratoria,
@@ -175,11 +231,13 @@ class Movimientos(db.Model):
 
 class Bitacora(db.Model):
     __tablename__ = 'bitacora'
+    __table_args__ = {"schema": "public"}
 
-    id_bitacora = db.Column(db.Integer, primary_key=True)
+    id_bitacora = db.Column(db.Integer, primary_key=True, autoincrement=True)
     fecha = db.Column(db.DateTime)
-    id_movimiento = db.Column(db.Integer)
+    id_movimiento = db.Column(db.Integer, db.ForeignKey('public.movimientos.id_movimiento'))
     tipo_movimiento = db.Column(db.Integer)
+
 
     def __init__(self, id_bitacora, fecha, id_movimiento, tipo_movimiento):
         self.fecha = fecha
@@ -189,14 +247,16 @@ class Bitacora(db.Model):
     def __repr__(self):
         return '<id_bitacora {}>'.format(self.id_bitacora)
 
-class Tipo_movimiento(db.Model):
-    __tablename__='tipo_movimiento'
 
-    id_tipo_movimiento = db.Column(db.Integer, primary_key=True)
-    descripcion = db.Column(db.String(70))
 
-    def __init__(self, descripcion):
-        self.descripcion = descripcion
+# class Tipo_movimiento(db.Model):
+#     __tablename__='tipo_movimiento'
 
-    def __repr__(self):
-            return '<id_tipo_movimiento {}>'.format(self.id_tipo_movimiento)
+#     id_tipo_movimiento = db.Column(db.Integer, primary_key=True)
+#     descripcion = db.Column(db.String(70))
+
+#     def __init__(self, descripcion):
+#         self.descripcion = descripcion
+
+#     def __repr__(self):
+#             return '<id_tipo_movimiento {}>'.format(self.id_tipo_movimiento)
